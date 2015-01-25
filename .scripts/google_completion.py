870ba2137196f6e998062d3c7336634379ce8f74
Yeonjun Lim <yjroot@gmail.com>
2015-01-26 02:17:25 +0900

    Added google starch command.

---
+++ .scripts/google_completion.py
@@ -0,0 +1,22 @@
+from urllib import urlencode, urlopen
+from json import loads as jloads
+from sys import argv
+import re
+
+
+def google_comlete(query):
+    params = urlencode({'client': 'firefox', 'q': query})
+    url = 'https://suggestqueries.google.com/complete/search?%s' % params
+    raw = urlopen(url).read()
+    return jloads(raw)[1]
+
+try:
+    query = re.sub(r'\\ |\\$', ' ', ' '.join(argv[1:]))
+
+    keyword_list = google_comlete(query)
+    keyword_list = [k for k in keyword_list if k[:len(query)] == query]
+    keyword_list = [k.replace(' ', '\\ ') for k in keyword_list]
+
+    print '\n'.join(keyword_list)
+except:
+    pass
